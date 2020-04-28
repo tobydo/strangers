963aa202f2b15296ede658f6ae2cc37389733373
emischaufeld <eschaufeld@gmail.com>
2020-04-27 23:13:51 -0400

    animation controller should be working

---
+++ tien/Assets/scripts/playAni.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAni : MonoBehaviour
+{
+    private Animator ani;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        ani = gameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Animate(string animationName, string lastAnimation){
+       
+      
+        Debug.Log(animationName);
+        ani.SetBool(animationName, true);
+        ani.SetBool(lastAnimation, false);
+     ani.Play(animationName);
+    }
+}
