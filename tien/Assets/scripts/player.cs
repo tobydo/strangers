6c858bb0ee5ba371c67da8873f58b86ed3ac5fa8
dotoby <dotoby@gmail.com>
2020-04-30 04:08:08 -0400

    a lot lol

---
+++ tien/Assets/scripts/player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+    private Animator anim;
+    public Transform roadAxis;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        bool moving = false;
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isWalkingBack", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalkingRight", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingBack", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalkingLeft", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingBack", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isWalkingBack", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingBack", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+    }
+}
---
+++ tien/Assets/scripts/trigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+class trigger : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+
+    private void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+  
+        flowchart.ExecuteBlock("streaming");
+    }
+}
