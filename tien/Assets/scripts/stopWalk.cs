06ca416c24d6be450f084f8f5241e52b7667c49e
emischaufeld <eschaufeld@gmail.com>
2020-05-10 21:53:03 -0400

    stop walking and audio listeners

---
+++ tien/Assets/scripts/stopWalk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stopWalk : MonoBehaviour
+{
+    public GameObject tien;
+    // Start is called before the first frame update
+
+
+    void StopWalk()
+    {
+        player Player = tien.GetComponent<player>();
+
+        Player.enabled = false;
+
+    }
+}
