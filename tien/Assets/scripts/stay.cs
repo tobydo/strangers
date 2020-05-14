8d831b79d9b925eea24df57898b887e1fa235e14
emischaufeld <eschaufeld@gmail.com>
2020-05-14 19:23:56 -0400

    sound destory

---
+++ tien/Assets/scripts/stay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class stay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+        if (scene.name == "Restaurant")
+        {
+            // Destroy the gameobject this script is attached to
+            Cursor.visible = true;
+            Destroy(gameObject);
+        }
+    }
+}
