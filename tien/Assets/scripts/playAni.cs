6f631193bc0c6e82e733c1d36a35f3d06c73f638
emischaufeld <eschaufeld@gmail.com>
2020-05-14 17:33:13 -0400

    credit scroll

--- tien/Assets/scripts/playAni.cs
+++ tien/Assets/scripts/playAni.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playAni : MonoBehaviour
 {
@@ -21,12 +22,31 @@ public class playAni : MonoBehaviour
     }
 
     public void Animate(string animationName, string lastAnimation){
-       
+       ani.enabled = true;
       
         Debug.Log(animationName);
         ani.SetBool(animationName, true);
         ani.SetBool(lastAnimation, false);
         ani.SetBool("isWalking", false);
      ani.Play(animationName);
+    }
+
+     public void AnimateText(string animationName, string lastAnimation){
+       ani.enabled = true;
+        Text texty = GetComponent<Text>();
+        texty.enabled = true;
+      
+        Debug.Log(animationName);
+        ani.SetBool(animationName, true);
+        ani.SetBool(lastAnimation, false);
+        ani.SetBool("isWalking", false);
+     ani.Play(animationName, -1, 0f);
+    }
+
+    public void StopIt(){
+        ani.enabled = false;
+       Text texty = GetComponent<Text>();
+
+       texty.enabled = false;
     }
 }
