8987e5c9afd63a8c009aa33824aa535fe2710fd1
emischaufeld <eschaufeld@gmail.com>
2020-05-25 18:00:30 -0400

    mouse works and creidts lower

---
+++ tien/Assets/scripts/cursorunlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorunlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake(){
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
